6507b83c8b19760a6d6689820294a7239cde385a
liuqiao1 <3504735@qq.com>
2017-07-25 16:44:51 +0800

    add images

---
+++ src/components/ImageFigure.js
@@ -0,0 +1,31 @@
+/*class ImageFigure extends React.Component {
+  render() {
+    return (
+        <figure>
+            <img src="" alt={}/>
+        </figure>
+    )
+  }
+}*/
+require('./ImageFigure.less');
+
+//如果不引入React：Uncaught ReferenceError: React is not defined
+import React from 'react'
+//import styles from './ImageFigure.less'
+
+const ImageFigure = ({...props}) => {
+  const {imageURL, title} = props;
+
+  return (
+    <figure className = 'img-figure'>
+        <img src={imageURL} alt={title}/>
+        <figcaption>
+            <h2>{title}</h2>
+        </figcaption>
+    </figure>
+  );
+}
+
+export default ImageFigure
+//import时如果写 import {ImageFigure}就错了
+//加了default 默认引入  import 随便名字 from ''
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,20 +2,60 @@ require('normalize.css/normalize.css');
 require('styles/App.less');
 
 import React from 'react';
+import ImageFigure from './ImageFigure'
 
-let yeomanImage = require('../images/yeoman.png');
+const imageDatas = require('../data/imgDatas.json');
+
+//let yeomanImage = require('../images/yeoman.png');
+//let imageDataArr = [];
+
+
+(function getImageURL(imageDatas){
+    imageDatas.map((item)=>{
+        //imageDataArr[key] = 
+        item.imageURL = require('../image/'+item.fileName);
+    });
+
+    //return imageDataArr;
+    //打印出来的路径/assets/01aff2240004563d8797f773c30c1ece.jpg  已经被webpack处理过了
+    //console.log(imageDataArr)
+})(imageDatas)
+
+//console.log(imageDatas);
+
+// function genImageFigures(){
+//   imageDataArr.map((item,key) => {
+//         const imageProps={
+//           imageURL:item,
+//           title:'test'
+//         }
+//        imageFigures.push(<ImageFigure {...imageProps} />);
+//   });
+// }
+
+
 
 class AppComponent extends React.Component {
+ 
   render() {
+    let imageFigures = [];
+
+    imageDatas.forEach(function(value){
+        const imageProps={
+          imageURL:value.imageURL,
+          title:value.title
+        }
+        imageFigures.push(<ImageFigure {...imageProps}/>);
+    });
+
     return (
       /*<div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />Hello
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>*/
-      <section className="stage">
-        stage
+      <section className="stage">  
         <section className="img-sec">
-          img-sec
+          {imageFigures}
         </section>
 
         <section className="ctrl-nav">
